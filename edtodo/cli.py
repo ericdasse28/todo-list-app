8d0500922aff9758f1e54b0ddca8da34b6587e8b
Eric Dasse <christ.gououi@gmail.com>
2023-09-19 13:39:37 +0200

    Feat: minimal CLI
    Only has support for `--help`, `-v` and `--version` options

--- edtodo/cli.py
+++ edtodo/cli.py
@@ -1,1 +1,27 @@
 """Provides the Typer command line interface for the application"""
+
+from typing import Optional
+import typer
+
+from edtodo import __app_name__, __version__
+
+app = typer.Typer()
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
