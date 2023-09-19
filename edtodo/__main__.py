b7e3bf2a2439fcaf2e02aaeb6085311a8342ad22
Eric Dasse <christ.gououi@gmail.com>
2023-09-19 13:51:55 +0200

    Feat: entrypoint code

--- edtodo/__main__.py
+++ edtodo/__main__.py
@@ -1,2 +1,12 @@
 """Entrypoint script to run the app from the package using the
 python -m edtodo command"""
+
+from edtodo import __app_name__, cli
+
+
+def main():
+    cli.app(prog_name=__app_name__)
+
+
+if __name__ == "__main__":
+    main()
